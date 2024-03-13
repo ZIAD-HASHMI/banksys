631fa65a5394c9b6183b2f3f564916d3fd8baf9b
ZIAD-HASHMI <162905240+ZIAD-HASHMI@users.noreply.github.com>
2024-03-13 22:29:17 +0300

    Update readme.php

--- readme.php
+++ readme.php
@@ -2,7 +2,8 @@ phone banksystem
 
 To instialize this system flow the steps 
 
--you need apache server or any server that can run php cript 
+-you need apache server or any server that can run php script 
+-apache 
 - PHPMYAdmin 
 -mysql sever 
 
